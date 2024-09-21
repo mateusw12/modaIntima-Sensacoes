e2caa50f31e31e5d6bc20510e8d18d2f8e07c9ee
Mateus Walz <mateus.walz@gmail.com>
2024-09-21 15:56:09 -0300

    Ajusta layout do input de pesqusa (#5)

--- irys-moda-intima-sensacoes/src/components/baseLayout/navbar/index.tsx
+++ irys-moda-intima-sensacoes/src/components/baseLayout/navbar/index.tsx
@@ -1,15 +1,17 @@
 import { CATEGORIES, MANAGEMENT_ITEMS } from "@/config/constant/menuConstant";
-import { InputSearch } from "@/shared/lib/input/input";
+import { Input, InputSearch } from "@/shared/lib/input/input";
 import styles from "@/styles/baseLayout.module.css";
 import {
   Box,
   Divider,
   Drawer,
   IconButton,
+  InputAdornment,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
+  TextField,
 } from "@mui/material";
 import { useState } from "react";
 import {
@@ -17,6 +19,7 @@ import {
   MdNotifications,
   MdOutlineAccountCircle,
   MdOutlineShoppingCart,
+  MdSearch,
 } from "react-icons/md";
 import Image from "next/image";
 
@@ -41,131 +44,145 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navContainer}>
-        <div className={styles.navColumnMenu}>
-          <IconButton
-            className={styles.menuIcon}
-            aria-label="menu"
-            size="large"
-            onClick={toggleDrawer(true)}
-          >
-            <MdMenu className={styles.iconStyle} />
-          </IconButton>
-        </div>
-
-        <div className={styles.navColumnLogo}>
-          <Image
-            className={styles.logo}
-            src="/assets/logo/logo.png"
-            alt="Logo"
-            width={200}
-            height={200}
-          />
-        </div>
-
-        <div className={styles.navColumnIconSearch}>
-          <div className={styles.icons}>
-            <IconButton aria-label="notifications" size="medium">
-              <MdNotifications className={styles.iconStyle} />
-            </IconButton>
-
-            <IconButton aria-label="account" size="medium">
-              <MdOutlineAccountCircle className={styles.iconStyle} />
-            </IconButton>
-
-            <IconButton aria-label="shop" size="medium">
-              <MdOutlineShoppingCart className={styles.iconStyle} />
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navContainer}>
+          <div className={styles.navColumnMenu}>
+            <IconButton
+              className={styles.menuIcon}
+              aria-label="menu"
+              size="large"
+              onClick={toggleDrawer(true)}
+            >
+              <MdMenu className={styles.iconStyle} />
             </IconButton>
           </div>
-          <InputSearch
-            class={styles.searchInput}
-            label={"Pesquisar..."}
-            onChange={handleSearch}
-          />
-        </div>
-      </div>
 
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <List>
-            {CATEGORIES.map((category) => (
-              <div
-                key={category.title}
-                className={styles.categoryContainer}
-                onMouseEnter={() => handleMouseEnter(category.title)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary={category.title} />
-                  </ListItemButton>
-                </ListItem>
+          <div className={styles.navColumnLogo}>
+            <Image
+              className={styles.logo}
+              src="/assets/logo/logo.png"
+              alt="Logo"
+              width={200}
+              height={200}
+            />
+          </div>
+
+          <div className={styles.navColumnIconSearch}>
+            <div className={styles.icons}>
+              <IconButton aria-label="notifications" size="medium">
+                <MdNotifications className={styles.iconStyle} />
+              </IconButton>
+
+              <IconButton aria-label="account" size="medium">
+                <MdOutlineAccountCircle className={styles.iconStyle} />
+              </IconButton>
+
+              <IconButton aria-label="shop" size="medium">
+                <MdOutlineShoppingCart className={styles.iconStyle} />
+              </IconButton>
+            </div>
+          </div>
+        </div>
+
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <List>
+              {CATEGORIES.map((category) => (
                 <div
-                  className={`${styles.subCategoryContainer} ${
-                    activeCategory === category.title
-                      ? styles.showSubCategories
-                      : ""
-                  }`}
+                  key={category.title}
+                  className={styles.categoryContainer}
+                  onMouseEnter={() => handleMouseEnter(category.title)}
+                  onMouseLeave={handleMouseLeave}
                 >
-                  {category.subCategories.map((subCategory) => (
-                    <ListItem
-                      key={subCategory}
-                      disablePadding
-                      className={styles.subCategoryItem}
-                    >
-                      <ListItemButton>
-                        <ListItemText primary={subCategory} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemText primary={category.title} />
+                    </ListItemButton>
+                  </ListItem>
+                  <div
+                    className={`${styles.subCategoryContainer} ${
+                      activeCategory === category.title
+                        ? styles.showSubCategories
+                        : ""
+                    }`}
+                  >
+                    {category.subCategories.map((subCategory) => (
+                      <ListItem
+                        key={subCategory}
+                        disablePadding
+                        className={styles.subCategoryItem}
+                      >
+                        <ListItemButton>
+                          <ListItemText primary={subCategory} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {MANAGEMENT_ITEMS.map((item) => (
-              <div
-                key={item.title}
-                className={styles.categoryContainer}
-                onMouseEnter={() => handleMouseEnter(item.title)}
-                onMouseLeave={handleMouseLeave}
-              >
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary={item.title} />
-                  </ListItemButton>
-                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {MANAGEMENT_ITEMS.map((item) => (
                 <div
-                  className={`${styles.subCategoryContainer} ${
-                    activeCategory === item.title
-                      ? styles.showSubCategories
-                      : ""
-                  }`}
+                  key={item.title}
+                  className={styles.categoryContainer}
+                  onMouseEnter={() => handleMouseEnter(item.title)}
+                  onMouseLeave={handleMouseLeave}
                 >
-                  {item.subItems.map((subItem) => (
-                    <ListItem
-                      key={subItem}
-                      disablePadding
-                      className={styles.subCategoryItem}
-                    >
-                      <ListItemButton>
-                        <ListItemText primary={subItem} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemText primary={item.title} />
+                    </ListItemButton>
+                  </ListItem>
+                  <div
+                    className={`${styles.subCategoryContainer} ${
+                      activeCategory === item.title
+                        ? styles.showSubCategories
+                        : ""
+                    }`}
+                  >
+                    {item.subItems.map((subItem) => (
+                      <ListItem
+                        key={subItem}
+                        disablePadding
+                        className={styles.subCategoryItem}
+                      >
+                        <ListItemButton>
+                          <ListItemText primary={subItem} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-    </nav>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+      </nav>
+      <TextField
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position={"start"}>
+                {
+                  <>
+                    <MdSearch />
+                  </>
+                }
+              </InputAdornment>
+            ),
+          },
+        }}
+        size="medium"
+        placeholder="Pesquisar..."
+      />
+    </>
   );
 };
 
