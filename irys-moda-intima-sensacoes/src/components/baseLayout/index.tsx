22175a492374923246f8b94806863bd86392c694
mateus.walz <mateus.walz@gmail.com>
2024-09-24 19:40:26 -0300

    Ajusta layout base para as paginas da aplicação

--- irys-moda-intima-sensacoes/src/components/baseLayout/index.tsx
+++ irys-moda-intima-sensacoes/src/components/baseLayout/index.tsx
@@ -3,13 +3,13 @@ import Navbar from "./navbar";
 import styles from "@/styles/baseLayout.module.css";
 import Header from "./header";
 
-export const BaseLayout = () => {
+export const BaseLayout = (props: any) => {
   return (
     <div className={styles.layout}>
       <Header />
       <Navbar />
       <div className={styles.main}>
-        <div className={styles.content}></div>
+        <div className={styles.content}>{props.children}</div>
       </div>
       <Footer />
     </div>
--- irys-moda-intima-sensacoes/src/pages/index.tsx
+++ irys-moda-intima-sensacoes/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import BaseLayout from "@/components/baseLayout";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,7 +11,13 @@ export default function Home() {
       <div
         style={{ width: "100%", overflowX: "hidden", boxSizing: "border-box" }}
       >
-        <BaseLayout />
+        <div style={{ padding: "20px" }}>
+          <h1>Bem-vindo à Irys Moda Íntima e Sensações!</h1>
+          <p>
+            Explore nossa coleção de moda íntima e acessórios para todos os
+            gostos e estilos.
+          </p>
+        </div>
       </div>
     </>
   );
