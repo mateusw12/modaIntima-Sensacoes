cc00d590d871c90b8b82f0d11ae65c77ea6e7533
mateus.walz <mateus.walz@gmail.com>
2024-09-28 22:42:24 -0300

    Implementa página de atendimento e envio automatico de EMAIL

---
+++ irys-moda-intima-sensacoes/src/pages/atendimento/index.tsx
@@ -0,0 +1,11 @@
+import CustomService from "@/components/customerService";
+
+const CustomServicePage = () => {
+  return (
+    <div>
+      <CustomService />
+    </div>
+  );
+};
+
+export default CustomServicePage;
